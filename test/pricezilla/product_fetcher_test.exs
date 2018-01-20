d54e32a320c52e81517290082bd893529d342026
Rafael Rocha <rafaelrochasilva@gmail.com>
2018-01-20 17:31:41 -0200

    Remove uncessary variable into product fetcher test

--- test/pricezilla/product_fetcher_test.exs
+++ test/pricezilla/product_fetcher_test.exs
@@ -5,8 +5,6 @@ defmodule Pricezilla.ProductFetcherTest do
   alias Pricezilla.FakeHttpClient
 
   test "returns a list of products" do
-    products = "{\"productRecords\":[{\"price\":\"$30.25\",\"name\":\"Nice Chair\",\"id\":123456,\"discontinued\":false,\"category\":\"home-furnishings\"},{\"price\":\"$43.77\",\"name\":\"Black & White TV\",\"id\":234567,\"discontinued\":true,\"category\":\"electronics\"}]}"
-
     expected_response = %{
       "productRecords" => [
         %{
