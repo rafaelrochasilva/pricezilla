fe91d891c129b5875fcc87b475cb5ff74961845c
Rafael Rocha <rafaelrochasilva@gmail.com>
2018-02-07 08:32:01 -0200

    Encode URI
    Use URI.encode_query to handle with the query params to be send to an
    external API. By this we can garantee that the params are being passed
    in the right format.

--- lib/pricezilla/product_fetcher.ex
+++ lib/pricezilla/product_fetcher.ex
@@ -11,37 +11,28 @@ defmodule Pricezilla.ProductFetcher do
   @spec get(atom) :: {:ok, map} | {:error, binary}
   def get(client \\ HTTPoison, url \\ url_with_query()) do
     case client.get(url) do
-      {:ok, %{status_code: 200, body: body}} -> {:ok, parse_response(body)}
+      {:ok, %{status_code: 200, body: body}} -> {:ok, Poison.decode!(body)}
       {:ok, %{status_code: _, body: body}} -> {:error, body}
       {:error, %{reason: reason}} -> {:error, reason}
     end
   end
 
-  defp parse_response(body) do
-    Poison.decode!(body)
-  end
-
   defp url_with_query do
-    url() <> query()
-  end
-
-  defp query do
-    "?api_key=#{token()}&start_date=#{start_date()}&end_date=#{end_date()}"
-  end
-
-  defp start_date do
-    Timex.shift(Timex.today, days: 1)
-  end
-
-  defp end_date do
-    Timex.today
-  end
-
-  defp token do
-    System.get_env("OMEGA_PRICING_API_KEY")
+    url()
+    |> URI.merge("?" <> query_params())
+    |> to_string
   end
 
   defp url do
     System.get_env("OMEGA_PRICING_API_URL")
+  end
+
+  defp query_params do
+    %{
+      api_key: System.get_env("OMEGA_PRICING_API_KEY"),
+      start_date: Timex.shift(Timex.today, days: 1),
+      end_date: Timex.today
+    }
+    |> URI.encode_query
   end
 end
